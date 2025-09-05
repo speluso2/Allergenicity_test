b8b433d29381e082cc988a31541b51e4bb37ea93
Silvia <88490062+speluso2@users.noreply.github.com>
2025-09-05 17:50:42 +0200

    Delete .devcontainer/.devcontainer directory

--- .devcontainer/.devcontainer/install.R
+++
@@ -1,9 +0,0 @@
-install.packages(
-  c("ggplot2", "dplyr"),
-  repos = "https://cloud.r-project.org"
-)
-
-# Example: install Bioconductor package if needed:
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# BiocManager::install("Biostrings")
